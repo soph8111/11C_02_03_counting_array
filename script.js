8033f81f4a5cff2a3ebf3f942226196a587b3fd3
Sophie Spang <soph8111@stud.kea.dk>
2021-08-31 14:18:27 +0200

    adds numbers to array with delay

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const array = [];
+let counter;
+
+init();
+
+function init() {
+    counter = 0;
+    loop();
+}
+
+function loop() {
+    counter++;
+    setTimeout(loop, 650);
+    array.push(counter);
+    console.log(array);
+}
