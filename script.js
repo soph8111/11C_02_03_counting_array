f7943248f298d0a9aa55709d577b6eacad7eb23a
Sophie Spang <soph8111@stud.kea.dk>
2021-09-02 07:56:24 +0200

    assignment finish

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ let counter;
 init();
 
 function init() {
-    counter = 0;
+    counter = -1;
     loop();
 }
 
 function loop() {
     counter++;
+
     setTimeout(loop, 650);
-    array.push(counter);
+    array.unshift(counter);
+
+    if (array.length > 9) {
+        array.pop();
+        }
+
     console.log(array);
 }
